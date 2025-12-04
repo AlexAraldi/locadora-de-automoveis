09f82d80912b5e1eca9b2609ac200fb15365d144
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-04 11:45:48 -0300

    rotas aninhadas de configuracao de combustivel

---
+++ client/src/app/components/configuracoes/configuracao.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { ListarConfiguracoes } from './listar/listar-configuracoes';
+
+export const configuracaoRoutes: Routes = [{ path: '', component: ListarConfiguracoes }];
---
+++ client/src/app/components/configuracoes/listar/listar-configuracoes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-configuracoes',
+  imports: [],
+  templateUrl: './listar-configuracoes.html'
+})
+export class ListarConfiguracoes {
+
+}
