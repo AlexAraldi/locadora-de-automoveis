ed9a65b31a3e4bec2d63f607c292e0396b7aa124
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-04 11:46:29 -0300

    rotas aninhadas de devolucao

---
+++ client/src/app/components/devolucoes/devolucao.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { ListarDevolucoes } from './listar/listar-devolucoes';
+
+export const devolucaoRoutes: Routes = [{ path: '', component: ListarDevolucoes }];
---
+++ client/src/app/components/devolucoes/listar/listar-devolucoes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-devolucoes',
+  imports: [],
+  templateUrl: './listar-devolucoes.html'
+})
+export class ListarDevolucoes {
+
+}
