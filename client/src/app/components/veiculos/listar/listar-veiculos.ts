0998326959792fb5adab05b3d22399f81bbbedf3
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-04 11:48:44 -0300

    rotas aninhadas de veiculo

---
+++ client/src/app/components/veiculos/listar/listar-veiculos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-veiculos',
+  imports: [],
+  templateUrl: './listar-veiculos.html'
+})
+export class ListarVeiculos {
+
+}
---
+++ client/src/app/components/veiculos/veiculo.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { ListarVeiculos } from './listar/listar-veiculos';
+
+export const veiculoRoutes: Routes = [{ path: '', component: ListarVeiculos }];
