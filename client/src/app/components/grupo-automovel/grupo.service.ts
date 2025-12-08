a712c1dbf196334f5e662ce3fbc7c43d6dd698db
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-08 08:31:01 -0300

    implementado conexão com API.

---
+++ client/src/app/components/grupo-automovel/grupo.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GrupoService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = environment.apiUrl + '/grupos';
+
+  public selecionarTodos(): Observable<object> {
+    const urlCompleto = this.apiUrl + '/todos';
+    return this.http.get(urlCompleto);
+  }
+}
--- client/src/app/components/grupo-automovel/listar/listar-grupos.ts
+++ client/src/app/components/grupo-automovel/listar/listar-grupos.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GrupoService } from '../grupo.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-listar-grupos',
-  imports: [],
-  templateUrl: './listar-grupos.html'
+  imports: [AsyncPipe],
+  templateUrl: './listar-grupos.html',
 })
 export class ListarGrupos {
-
+  protected readonly grupoService = inject(GrupoService);
+  protected readonly grupos$ = this.grupoService.selecionarTodos();
 }
