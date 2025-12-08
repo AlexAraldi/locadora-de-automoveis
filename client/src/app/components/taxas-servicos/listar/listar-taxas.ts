84a300b5ef8ff79a4ea4ee76285fda1c5c3de664
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-08 08:31:45 -0300

    implementado conexão com API.

--- client/src/app/components/taxas-servicos/listar/listar-taxas.ts
+++ client/src/app/components/taxas-servicos/listar/listar-taxas.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { A } from '@angular/cdk/keycodes';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { TaxaService } from '../taxa.service';
 
 @Component({
   selector: 'app-listar-taxas',
-  imports: [],
-  templateUrl: './listar-taxas.html'
+  imports: [AsyncPipe],
+  templateUrl: './listar-taxas.html',
 })
 export class ListarTaxas {
-
+  protected readonly taxaService = inject(TaxaService);
+  protected readonly taxas$ = this.taxaService.selecionarTodos();
 }
