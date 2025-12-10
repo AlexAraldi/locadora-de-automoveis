008491d95415cd02265c065a60bddbd6409fb5ae
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-10 11:02:45 -0300

    implementado Listar alugueis

--- client/src/app/components/alugueis/listar/listar-alugueis.ts
+++ client/src/app/components/alugueis/listar/listar-alugueis.ts
@@ -1,10 +1,13 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { AluguelService } from '../aluguel.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-alugueis',
-  imports: [AsyncPipe],
+  imports: [MatButtonModule, MatIconModule, RouterLink, AsyncPipe],
   templateUrl: './listar-alugueis.html',
 })
 export class ListarAlugueis {
