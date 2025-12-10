a97835b806c7a997490ed487a1baa05c20521b86
AlexAraldi <alexanderaraldi@gmail.com>
2025-12-10 11:05:01 -0300

    Implementado listar clientes e condutores

--- client/src/app/components/clientes/listar/listar-clientes.ts
+++ client/src/app/components/clientes/listar/listar-clientes.ts
@@ -1,10 +1,13 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ClienteService } from '../cliente.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-clientes',
-  imports: [AsyncPipe],
+  imports: [MatButtonModule, MatIconModule, RouterLink, AsyncPipe],
   templateUrl: './listar-clientes.html',
 })
 export class ListarClientes {
--- client/src/app/components/condutores/listar/listar-condutores.ts
+++ client/src/app/components/condutores/listar/listar-condutores.ts
@@ -1,10 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { CondutorService } from '../condutor.service';
 import { AsyncPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-condutores',
-  imports: [AsyncPipe],
+  imports: [MatButtonModule, MatIconModule, RouterLink, AsyncPipe],
+
   templateUrl: './listar-condutores.html',
 })
 export class ListarCondutores {
